a06fd70e515cd0970fce7a55fc791344597dcfed
shenghui_li <shenghui_li9488@163.com>
2021-06-10 16:06:43 +0800

    Header,List,FormValidation components

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,91 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <global-header :user="user"></global-header>
+    <!-- <column-list :list ="list"></column-list>  -->
+    <form>
+      <div class="mb-3">
+        <label class="form-label">Email adress</label>
+        <validate-input :rules="emailRules" v-model="emailVal"></validate-input>
+        {{emailVal}}
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+      </div>
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
+import { defineComponent, reactive, ref } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
+import ValidateInput, { RulesProp } from './components/ValidateInput.vue'
+// const testData: ColumnProps[] = [
+//   {
+//     id: 1,
+//     title: 'test1的专栏',
+//     description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
+//     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+//   },
+//   {
+//     id: 2,
+//     title: 'test2的专栏',
+//     description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+//     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+//   },
+//   {
+//     id: 2,
+//     title: 'test2的专栏',
+//     description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧'
+//     // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+//   },
+//   {
+//     id: 2,
+//     title: 'test2的专栏',
+//     description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+//     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+//   }
+// ]
+const userInfo: UserProps = {
+  isLogin: true,
+  name: 'pogo',
+  id: 3
+}
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    // ColumnList,
+    GlobalHeader,
+    ValidateInput
+  },
+  setup () {
+    const emailVal = ref('viking')
+    const emailRules: RulesProp = [
+      { type: 'required', message: 'Email must not be empty!' },
+      { type: 'email', message: 'email format must be correct!' }
+    ]
+    return {
+      emailRules: emailRules,
+      // list: testData,
+      user: userInfo,
+      emailVal
+    }
   }
 })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
